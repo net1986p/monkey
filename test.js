bcc0b5070a841465aa81fc34c1611e403516d5b0
net1986p <admin@qingdao-seo.net>
2024-11-27 07:56:52 +0800

    test

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log('223')
+// ==UserScript==
+// @name         Mytest
+// @namespace    http://www.qingdao-seo.net/
+// @version      2024-11-26
+// @description  脚本的描述信息
+// @author       colors
+// @match        https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=git-scm.com
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('test!!!!!');
+    // Your code here...
+})();
