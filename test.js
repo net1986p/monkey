b5d92dcb52246ff3740193ae77312310261ed3c3
net1986p <admin@qingdao-seo.net>
2024-11-27 08:51:15 +0800

    v1.1

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Mytest
 // @namespace    http://www.qingdao-seo.net/
-// @version      2024-11-26
+// @version      1.1
 // @description  脚本的描述信息
-// @author       colors
-// @match        https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+// @author       colors lightiing
+// @match        https://www.qingdao-seo.net/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=git-scm.com
 // @grant        none
 // @license      MIT
